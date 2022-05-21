6e6dc238224775ff17c38ba8a835b313d67024c0
daverydan <danny.w.avery@gmail.com>
2022-05-21 14:32:29 -0400

    +default layout

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="layout-no-header-full">
+    <main class="content overflow-auto">
+      <nuxt />
+    </main>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+html {
+  height: 100%;
+  background: #eee;
+}
+
+body {
+  height: 100%;
+}
+
+.toasted-container.top-center {
+  top: 5% !important;
+}
+
+.toasted.toasted-primary.info {
+  background: #367cff !important;
+}
+</style>
